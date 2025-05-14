8df5a979540bb798a986057304811247124faeca
2327cse1156 <anshkaushal92@gmail.com>
2025-05-15 02:15:19 +0530

    basic backend server setup

---
+++ Brainwave_backend/controllers/userController.js
@@ -0,0 +1,61 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Validate Input
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Hash Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create User with Hashed Password
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    // Send Response with User and Token
+    res.status(201).json({
+      success: true,
+      message: "Account created successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token, // Include the JWT token for authentication
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
---
+++ Brainwave_backend/models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique:true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength:6
+    },
+    role: {
+      type: String,
+      enum: ["Student", "Instructor"],
+      default: "Student",
+    },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    photoURL: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
